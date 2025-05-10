68d9fe9bf140dcfb3c2781cd6dc676fa7aef0056
today100 <ruoshui.grace.zhang@gmail.com>
2025-05-09 22:42:59 -0400

    update main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -8,6 +8,7 @@ const MotionImg = motion.img;
 const MotionDiv = motion.div;
 const MotionH1 = motion.h1;
 const MotionH2 = motion.h2;
+const MotionH3 = motion.h3;
 const MotionButton = motion.button;
 
 
@@ -70,43 +71,117 @@ const Home = () => {
         transition={{ duration: 1 }}
       >
         <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">
-          Advocating for social justice by educating, empowering, and inspiring a community of transformative leaders.
+        "Young people, when informed and empowered, when they realize that what they do truly makes a difference, can indeed change the world."
+        — Jane Goodall
         </h2>
         </MotionMain>
 
 
-      <span className="block h-px mx-0 my-5 p-0 border-t-[#ccc] border-0 border-t border-solid"></span>
+      {/* <span className="block h-px mx-0 my-5 p-0 border-t-[#ccc] border-0 border-t border-solid bg-gray-white"></span> */}
 
       <MotionMain
-        className='p-5 md:p-10 flex flex-col lg:flex-row justify-center lg:justify-between items-center text-center'
+        className='p-5 md:p-10 block lg:flex-row justify-center lg:justify-between items-center text-center bg-gray-white'
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true }}
         transition={{ duration: 1 }}
       >
-        <MotionImg
-          className='w-full md:w-1/2 lg:w-1/3 h-auto m-5'
-          src="kids.webp"
-          alt="Kids"
-          initial={{ x: -100, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
-        />
+        <MotionH2 className="text-3xl md:text-4xl lg:text-5xl font-bold pb-5 text-red-900 pt-12"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}>
+          Our Belief
+        </MotionH2>
 
-        <MotionDiv
-          className='p-5 mx-0 md:mx-10 w-full md:w-2/3 text-red-900'
-          initial={{ x: 100, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
+        <MotionMain
+          className='p-5 md:p-10 flex flex-col lg:flex-row justify-center lg:justify-between items-center text-center'
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
           transition={{ duration: 1 }}
         >
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold pb-5">Opportunities for Students by Students</h2>
-          <div className='p-2 md:p-5 text-lg md:text-xl lg:text-2xl'>
-            <p>Become a part of the MCYC team and earn volunteer hours, while contributing to a cause that you believe in.</p>
-          </div>
-        </MotionDiv>
+          
+          <MotionDiv
+            className='mx-0 md:mx-10 w-full md:w-2/3 text-left'
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+          >
+            <div className='p-2 md:p-5 text-lg md:text-xl lg:text-2xl'>
+              <p>
+              The Movement for Change Youth Council (MCYC) believes that young people are not just preparing to lead—they’re leading now. Too often, traditional systems overlook youth voices and potential. MCYC exists to change that. 
+<br /><br />
+Through advocacy, community initiatives, and leadership development, we create a space where youth are empowered to speak out, take action, and drive real change. By investing in youth today, we’re cultivating the leaders, innovators, and changemakers of tomorrow.
+              </p>
+            </div>
+          </MotionDiv>
+          
+          <MotionImg
+            className='w-full md:w-1/2 lg:w-1/3 h-auto m-5'
+            src="kids.webp"
+            alt="Kids"
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+          />
+
+        </MotionMain>
       </MotionMain>
+
+      <MotionMain
+        className='p-5 md:p-10 block lg:flex-row justify-center lg:justify-between items-center text-center bg-white'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+      <MotionH2 className="text-3xl md:text-4xl lg:text-5xl font-bold pb-5 text-red-900 pt-12"
+         initial={{ y: -50, opacity: 0 }}
+         animate={{ y: 0, opacity: 1 }}
+         transition={{ delay: 0.5, duration: 1 }}>Our Impact</MotionH2>
+
+      </MotionMain>
+
+      <MotionMain
+        className='p-5 md:p-10 block lg:flex-row justify-center lg:justify-between items-center text-center bg-gray-white'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+      <MotionH2 className="text-3xl md:text-4xl lg:text-5xl font-bold pb-5 text-red-900 pt-12"
+         initial={{ y: -50, opacity: 0 }}
+         animate={{ y: 0, opacity: 1 }}
+         transition={{ delay: 0.5, duration: 1 }}>Past Event</MotionH2>
+
+      </MotionMain>
+
+      <MotionMain
+        className='p-5 md:p-10 block lg:flex-row justify-center lg:justify-between items-center text-center bg-white'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+      <MotionH3 className="text-5xl md:text-4xl lg:text-5xl font-bold pb-5 text-red-900 pt-12"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}>
+        
+        A big thanks to our Sponsors!
+        <br />
+        <br />
+       
+        Checkout our Instagram to stay updated and be part of the change!
+         
+      </MotionH3>
+
+      
+
+      </MotionMain>
+
 
       {/* <Footer /> */}
     </div>
